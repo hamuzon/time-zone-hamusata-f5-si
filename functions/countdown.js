b54bf36dc441f481e0a5ed04ffc3bc5d65b71e85
hamuzon <nagasawa.1399@gmail.com>
2025-09-30 01:22:43 +0900

    v1.0.0 countdown.js

--- functions/countdown.js
+++ functions/countdown.js
@@ -1,19 +1,16 @@
 // functions/countdown.js
 
+import win10Html from "win10.html?raw";
+import newyearHtml from "newyear.html?raw";
+
+const htmlFiles = {
+  "/countdown/win10.html": win10Html,
+  "/countdown/newyear.html": newyearHtml,
+};
+
 export async function onRequest(context) {
   const url = new URL(context.request.url);
   const pathname = url.pathname;
-
-  // public 内のHTMLを静的 import
-  const htmlFiles = {
-    "/countdown/win10.html": `<!DOCTYPE html>
-<html><head><title>Windows 10 Countdown</title></head>
-<body><h1>Windows 10 Countdown Page</h1></body></html>`,
-
-    "/countdown/newyear.html": `<!DOCTYPE html>
-<html><head><title>New Year Countdown</title></head>
-<body><h1>New Year Countdown Page</h1></body></html>`,
-  };
 
   if (pathname in htmlFiles) {
     return new Response(htmlFiles[pathname], {
